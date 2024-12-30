28c724161b1cc7841881c79027158b90f10530a9
summa <summersad10@gmail.com>
2024-12-30 15:46:16 +0900

    결제완료

--- src/main/java/com/example/project/repository/reserve/SeatRepository.java
+++ src/main/java/com/example/project/repository/reserve/SeatRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.project.entity.reserve.Seat;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
-    // List<Seat> getAuditoriumSeat(Long auditoriumNo);
+
 }
