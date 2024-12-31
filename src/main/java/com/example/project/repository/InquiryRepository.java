99dbd10455fb8566b4cd4e348e968baeef650046
gyowoong <landjjang1212@gmail.com>
2024-12-31 15:19:36 +0900

    답변 및 미답변 코드 수정

--- src/main/java/com/example/project/repository/InquiryRepository.java
+++ src/main/java/com/example/project/repository/InquiryRepository.java
@@ -1,6 +1,7 @@
 package com.example.project.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import com.example.project.entity.Inquiry;
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
     List<Inquiry> findByUsername(String username);
+
+    Optional<Inquiry> findById(Long id);
 }
